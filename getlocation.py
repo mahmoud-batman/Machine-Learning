6f5f50d5a1458e3b33fb494143a8a8a6f4d3a060
Mahmoud mostafa <30936008+mahmoud-batman@users.noreply.github.com>
2017-08-14 00:47:00 +0200

    Update getlocation.py

--- getlocation.py
+++ getlocation.py
@@ -11,7 +11,7 @@ if __name__== "__main__":
     graph = facebook.GraphAPI(access_token='EAACEdEose0cBAC1rZBmVaZC6ExQcgwGRUnUYgy2UFB1LN3JN0DxSwqIHdavN2HWJZAC11O9aUrxKFfN8lZBmVSuwjSei4M8VmpP3qDZBZChbvmhUOQXOZAaT1BBqVCQUA8iJB8IhZBZBN3LBso1JiRkn5knclEZAEBCOFM98M5iqFkF7hGZC5qZBANPwksCgPFtb8hGuuXOmm1KZAVwZDZD'
                               , version='2.7')
     
-    profile = graph.get_object(id= 'me',fields = "name , location{location} ,friends")
+    profile = graph.get_object(id= 'me',fields = "name , location{location}")
     friends = graph . get_connections ( id = 'me' , connection_name = 'friends' )
             
     print(json.dumps(friends, indent = 2))
