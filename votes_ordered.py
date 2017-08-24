7b69d9d96242b3bb3856b96d17636a74d9c3df65
Mahmoud mostafa <30936008+mahmoud-batman@users.noreply.github.com>
2017-08-25 00:03:05 +0200

    Update votes_ordered.py

--- votes_ordered.py
+++ votes_ordered.py
@@ -7,30 +7,48 @@ source = "E:\\WORK\\PROGRAMMER\\omr_tasks\\Votes\\Votes.txt"
 destination = 'E:\\WORK\\PROGRAMMER\\omr_tasks\\Votes\\Votes_ordered.csv'
 
 def vote_ordered(src, dest):
-    src = source
+    #open the name list file votes.txt 
     file = open(src, 'r') 
     names = file.read()
+    #replace the new line with space 
     names =names.replace('\n' ,',').split(',')
+    #remove the last empty character 
     names  = names[:-1]
+    #close the file 
     file.close()
+    # creates a dictionary of the name(key) and it frequency (value)
     freqs = Counter(names)
+    
+    # here we have three levels of sorting 
+    ''' first sort (the outer) : is to sort by the frequency (values):
+            supposed to be like that >> sorted(freq.items(), key = lambda p: p[1] , reverse=True))
+            but first sort must be sorted by the first name also so we do the second sort.
+        second sort (the middle) : 
+            supposed to be like that >> sorted(freq.items(), key = lambda p: p[0].split(" ")[0]))
+            but second sort must be sorted  by the second name also so we do the third sort.
+        third sort (the inner) :
+            supposed to be like that >> sorted(freq.items(), key = lambda p: p[0].split(" ")[1]))
+    so we put each level inside the other as we mentioned 
+    '''
     sorted_count = sorted(
                         sorted(
                                 sorted(freqs.items(),  
                                            key = lambda p: p[0].split(" ")[1] ),  
                                                key = lambda p: p[0].split(" ")[0]) ,
                                                   key = lambda p: p[1] , reverse=True)
-
+    #sorted_count is a list of tuples so we put it in a dataframe  
     df = pd.DataFrame(sorted_count)
-    dest = destination
+    #convert the dataframe into a csv file 
     df.to_csv(dest, header=None , index=None , sep=' ', mode='a')
     
-    alphab = list(df[0])
-    frequencies = list(df[1])
+    # ploting the names to its frequency
+    alphab = list(df[0]) #names
+    frequencies = list(df[1])#freq
     pos = np.arange(len(alphab))
-    width = 1.0     # gives histogram aspect to the bar diagram
+    width = 1  
     ax = plt.axes()
     ax.set_xticks(pos + (width / 2) )
     ax.set_xticklabels(alphab ,rotation = 90, fontsize = 8 ,color='blue', fontweight='bold',alpha=0.8)
     plt.bar(pos, frequencies, width, color='rgb')
     plt.show()
+    
