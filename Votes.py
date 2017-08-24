d14c2a2e3a59b7b896e44f0c51c24a66116e57ba
Mahmoud mostafa <30936008+mahmoud-batman@users.noreply.github.com>
2017-08-24 19:28:23 +0200

    Update Votes.py

--- Votes.py
+++ Votes.py
@@ -1,16 +1,17 @@
 import random 
 
 lst = ["Omar","Mahmoud","Mosata","Ahmed","Yassin","Jone","zyad","walid"]
-address = "E:\\WORK\\PROGRAMMER\\omr_tasks\\"
+address = "E:\\WORK\\PROGRAMMER\\omr_tasks\\Votes\\"
+
 def random_names( lstOFNames ,address , lenOflst ):    
-    file = open("Votes.txt" , 'a')
+    file = open(""+address+"Votes.txt" , 'a')
     while lenOflst > 0 :
     
         print(lstOFNames[random.randint(0,(len(lstOFNames)-1))] + " " 
                          + lstOFNames[random.randint(0,(len(lstOFNames)-1))])
         
         file.write(lstOFNames[random.randint(0,(len(lstOFNames)-1))] + " " 
-                         + lstOFNames[random.randint(0,(len(lstOFNames)-1))]+"\n")
+                         + lstOFNames[random.randint(0,(len(lstOFNames)-1))] +"\n")
         
         lenOflst -= 1
         
