65141f8796ad95efff559f39de14384ca998f7d9
Mahmoud mostafa <30936008+mahmoud-batman@users.noreply.github.com>
2017-08-25 16:53:05 +0200

    Update flight-plan.py

--- flight-plan.py
+++ flight-plan.py
@@ -41,28 +41,67 @@ annual("United Arab Emirates" , "United States")
 
 
 """
-top 10 countries
+top visitor countries
 """
-def top_visitor(country , year ):
+def top_visitor(country , year , ntop ):
+
     i = int(year) - 2010
     data1 = data[i][data[i]["DEST_COUNTRY_NAME"] == country ]
     data_sorted = data1.sort_values(["count"] , ascending = False)    
-    print(data_sorted[0:10])
-    dt = list(data_sorted[0:10]["ORIGIN_COUNTRY_NAME"])
-    lst = list(data_sorted[0:10]["count"])
-    x = len(data_sorted[0:10])
+    
+    #this in case you choose the united state to avoid the united state from the graph
+    data_sorted = data_sorted[1: ]
+    
+   # print(data_sorted[0:127])
+    dt = list(data_sorted[0:int(ntop)]["ORIGIN_COUNTRY_NAME"])
+    lst = list(data_sorted[0:int(ntop)]["count"])
+    x = len(data_sorted[0:int(ntop)])
     x_lst = []  
     for i in range(x) :
         x_lst.append(i)
-    plt.xticks(x_lst, dt , rotation = '45' , fontsize = 8)
+    plt.xticks(x_lst, dt , rotation = '90' , fontsize = 8)
     plt.plot(x_lst , normalize(lst) , 'o-')
-    plt.title("top_visitors to "+str(country)+"")
+    plt.title("top_"+str(ntop)+" visitors to "+str(country)+" in "+str(year)+"")
     
 def normalize(lst):
     return list(map(lambda x : x / sum(lst) , lst))
 
-top_visitor("United States" , 2011)
+top_visitor("United States" , 2010 , 20)
 
- 
- 
+"""
+top direction countries
+"""
+def top_directions(country , year , ntop ):
+ #  country = 'Egypt'
+    i = int(year) - 2010
+    data1 = data[i][data[i]["ORIGIN_COUNTRY_NAME"] == country ]
+    data_sorted = data1.sort_values(["count"] , ascending = False)    
+    
+    #this in case you choose the united state to avoid the united state from the graph
+    data_sorted = data_sorted[1: ]
+    
+    dt = list(data_sorted[0:int(ntop)]["DEST_COUNTRY_NAME"])
+    lst = list(data_sorted[0:int(ntop)]["count"])
+    x = len(data_sorted[0:int(ntop)])
+    x_lst = []  
+    for i in range(x) :
+        x_lst.append(i)
+    
+    
+    plt.xticks(x_lst, dt , rotation = '90' , fontsize = 8)
+    plt.plot(x_lst , normalize(lst) , 'o-')
+    plt.title("top_"+str(ntop)+" directions from "+str(country)+" in "+str(year)+"")
+    
+    
+top_directions("United States" , 2011, 20)
+
+
+    
+    
+    
+    
+    
+    
+
+
  
